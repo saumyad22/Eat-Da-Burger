95196f8ce8e02a117eeb064c101a04fb56bbbf20
Saumya <kdevara.saumya@gmail.com>
2017-08-13 17:39:57 -0700

    adding files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name) VALUES ('Veggie CheeseBurger');
+INSERT INTO burgers(burger_name) VALUES ('Black Bean Burger');
+INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers(burger_name) VALUES ('Chicken Cheeseburger');
